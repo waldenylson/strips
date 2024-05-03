89f41dc2543de2273a7ee9b661376c02a3fdc2dd
Waldenylson Silva <waldenylson@gmail.com>
2024-05-03 00:08:48 -0300

    fix: searchfield form submit button

--- app/components/Sectors/index.tsx
+++ app/components/Sectors/index.tsx
@@ -1,4 +1,5 @@
 import ActiveSectorsSelection from "../ActiveSectorsSelection";
+import SearchField from "../SearchField";
 import SpeedDialTootipMenu from "../SpeedDialTootipMenu";
 
 export const handleLayoutRestore = () => {
@@ -7,29 +8,33 @@ export const handleLayoutRestore = () => {
 };
 
 const Sectors: React.FC = () => {
+  const handleSearch = (value: string) => {
+    console.log("Valor de pesquisa:", value);
+  };
+
   return (
     <>
-      <SpeedDialTootipMenu />
-
       <div className="bottom-right-box-grid">
         <div className="bottom-right-box-grid-col-1">
           <ActiveSectorsSelection />
         </div>
         <div className="bottom-right-box-grid-col-2">
           {/* <span className="inline-flex items-center rounded-md  bottom-right-box-grid-col-2-bg-sector px-3 mx-1 py-1 text-xl font-medium text-black ring-1 ring-inset">
-          S2
-        </span>
-        <span className="inline-flex items-center rounded-md  bottom-right-box-grid-col-2-bg-sector px-3 mx-1 py-1 text-xl font-medium text-black ring-1 ring-inset">
-          S3
-        </span>
-        <span className="inline-flex items-center rounded-md bg-green-500 px-3 mx-1 py-1 text-xl font-medium text-black ring-1 ring-inset ring-green-500/10">
+            S2
+          </span>
+          <span className="inline-flex items-center rounded-md  bottom-right-box-grid-col-2-bg-sector px-3 mx-1 py-1 text-xl font-medium text-black ring-1 ring-inset">
+            S3
+          </span> */}
+          {/*<span className="inline-flex items-center rounded-md bg-green-500 px-3 mx-1 py-1 text-xl font-medium text-black ring-1 ring-inset ring-green-500/10">
           S4
         </span>
         <span className="inline-flex items-center rounded-md bg-green-500 px-3 mx-1 py-1 text-xl font-medium text-black ring-1 ring-inset ring-green-500/10">
           S5
         </span> */}
         </div>
+        <SpeedDialTootipMenu />
       </div>
+      <SearchField />
     </>
   );
 };
